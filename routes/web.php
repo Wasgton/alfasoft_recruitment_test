de43f56f1842e2672bc21d4d4f9c5a96ec254cbb
junior <wasgton.it@gmail.com>
2023-05-27 10:09:02 -0300

    build(Breeze): Installing laravel breeze to handle login and registration
    Installing laravel breeze and configuring views.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ContactsController;
+use App\Http\Controllers\HomePageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+require __DIR__.'/auth.php';
+
+Route::get('/', [ContactsController::class, 'index'])->name('home');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
