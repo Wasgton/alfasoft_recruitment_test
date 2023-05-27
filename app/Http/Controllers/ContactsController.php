ba5342b18a3fa04fcf8db85602465a8219edb0e0
junior <wasgton.it@gmail.com>
2023-05-27 10:06:42 -0300

    :sparkles: feat(Contacts): Starging Contacts Crud
    Creating migration, controller, model and factory for contact.

---
+++ app/Http/Controllers/ContactsController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Contact;
+use Illuminate\Http\Request;
+
+class ContactsController extends Controller
+{
+    public function index()
+    {
+        $contacts = Contact::paginate(20);
+        return view('home', compact('contacts'));
+    }
+}
---
+++ database/factories/ContactFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class ContactFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'contact' => $this->faker->regexify('[0-9]{9}'),
+        ];
+    }
+}
